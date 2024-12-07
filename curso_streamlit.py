007b94aaabb726ba5372d671cba8d8fb42ab9845
GuilhermeFer29 <leciguilherme@gmail.com>
2024-12-07 15:52:04 -0300

    selecao_album
    Adicionado a selecao de artista e album para mostrar dados das musicas mais tocadas daquele album

--- curso_streamlit.py
+++ curso_streamlit.py
@@ -10,8 +10,18 @@ df = pd.read_csv('01 Spotify.csv')
 
 df.set_index("Track", inplace=True)
 
+#Select Box seleção de artista
 artists = df['Artist'].value_counts().index
 option = st.selectbox('Artista',artists )
+df_filtered = df[df['Artist'] == option ]
+#seleção de albuns
 
-df_filter = df[df['Artist'] == option ]
-st.bar_chart(df_filter["Stream"])
+album = df_filtered['Album'].value_counts().index
+
+artist  = st.selectbox('Album', album)
+
+#side bar ocultar/mostrar Dados
+
+artist_data = st.checkbox("Dados Artista")
+if artist_data:
+    st.bar_chart(df_filtered["Stream"])
